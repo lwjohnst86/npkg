1ea22a3f9cda84963f007bab5634265419aaa1db
Luke Johnston <lwjohnst@gmail.com>
2019-02-08 13:34:57 +0100

    update to using usethis instead

--- R/make.R
+++ R/make.R
@@ -1,15 +1,34 @@
 make <- function(name) {
-    devtools::create(name)
-    devtools::use_git(pkg = name)
-    devtools::use_mit_license(name)
-    devtools::use_readme_rmd(name)
-    devtools::use_vignette(name, pkg = name)
+    usethis::create_package(name)
+    withr::with_dir(
+        name,
+        {
+            usethis::use_git()
+            usethis::use_mit_license()
+            usethis::use_readme_rmd()
+            usethis::use_vignette(name)
+            usethis::use_blank_slate()
+            usethis::use_code_of_conduct()
+            usethis::use_lifecycle_badge("experimental")
+        }
+    )
 }
 
-enhance <- function(pkg = '.') {
-    devtools::use_testthat(pkg)
-    devtools::use_cran_comments(pkg)
-    devtools::use_news_md(pkg)
-    devtools::use_github(pkg = pkg)
-    devtools::use_travis(pkg)
+enhance <- function() {
+    usethis::use_testthat()
+    usethis::use_cran_comments()
+    usethis::use_news_md()
+    usethis::use_github()
+    usethis::use_travis()
+    usethis::use_appveyor()
+    usethis::use_roxygen_md()
+    usethis::use_package_doc()
+}
+
+tidy_enhance <- function() {
+    # Add enhancement to fit with tidyverse guidelines
+}
+
+pkg_check <- function() {
+    # Run common checks on packages before releasing
 }
